9a9a22f399b07432bbd6ae15b2e59f3ac77ae098
Arsalan0736 <makhajankararsalan54@gmail.com>
2025-12-05 21:53:26 +0530

    Cloud Storage, PDF-Image conversion, Key-Value storage and AI feedback

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { usePuterStore } from "~/lib/puter";
-import { useLocation, useNavigate } from "react-router";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
-    { title: 'TrueResume | Auth' },
+    { title: 'Resumind | Auth' },
     { name: 'description', content: 'Log into your account' },
 ])
 
@@ -18,7 +18,7 @@ const Auth = () => {
     }, [auth.isAuthenticated, next])
 
     return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
@@ -50,4 +50,4 @@ const Auth = () => {
     )
 }
 
-export default Auth;
+export default Auth
